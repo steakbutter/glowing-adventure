fbb5304ded2b4336b2aa6743f8512119102ab79d
Kahlium <kahligtz@gmail.com>
2024-08-22 20:56:46 -0600

    testing out css components

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,48 +10,48 @@ module.exports = {
         ],
     },
     theme: {
-        mytheme: {
+        // mytheme: {
 
-            "primary": "#fde047",
+        //     "primary": "#fde047",
 
-            "primary-content": "#161202",
+        //     "primary-content": "#161202",
 
-            "secondary": "#eab308",
+        //     "secondary": "#eab308",
 
-            "secondary-content": "#130c00",
+        //     "secondary-content": "#130c00",
 
-            "accent": "#d97706",
+        //     "accent": "#d97706",
 
-            "accent-content": "#110500",
+        //     "accent-content": "#110500",
 
-            "neutral": "#374151",
+        //     "neutral": "#374151",
 
-            "neutral-content": "#d3d6da",
+        //     "neutral-content": "#d3d6da",
 
-            "base-100": "#374151",
+        //     "base-100": "#374151",
 
-            "base-200": "#2e3745",
+        //     "base-200": "#2e3745",
 
-            "base-300": "#262e3a",
+        //     "base-300": "#262e3a",
 
-            "base-content": "#d3d6da",
+        //     "base-content": "#d3d6da",
 
-            "info": "#d6d3d1",
+        //     "info": "#d6d3d1",
 
-            "info-content": "#101010",
+        //     "info-content": "#101010",
 
-            "success": "#a3e635",
+        //     "success": "#a3e635",
 
-            "success-content": "#0a1301",
+        //     "success-content": "#0a1301",
 
-            "warning": "#ef4444",
+        //     "warning": "#ef4444",
 
-            "warning-content": "#140202",
+        //     "warning-content": "#140202",
 
-            "error": "#ef4444",
+        //     "error": "#ef4444",
 
-            "error-content": "#140202",
-        },
+        //     "error-content": "#140202",
+        // },
     },
     plugins: [
         require('daisyui'),
